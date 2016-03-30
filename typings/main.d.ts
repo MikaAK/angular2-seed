219fc03296f6116a36f099d566699e746126b709
MikaAK <me@mikakalathil.ca>
2016-03-30 10:48:19 -0700

    Remove typings and add to gitignore

--- typings/main.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="main/ambient/core-js/core-js.d.ts" />
-/// <reference path="main/ambient/es6-shim/es6-shim.d.ts" />
-/// <reference path="main/ambient/jasmine/jasmine.d.ts" />
-/// <reference path="main/ambient/lodash/lodash.d.ts" />
-/// <reference path="main/ambient/require/require.d.ts" />
-/// <reference path="main/ambient/webfontloader/webfontloader.d.ts" />
-/// <reference path="main/definitions/es6-promise/es6-promise.d.ts" />
