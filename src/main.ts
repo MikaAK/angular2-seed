b30c1290ba0557e224b7c8045854be5c2a53c148
MikaAK <me@mikakalathil.ca>
2016-09-14 01:41:39 -0400

    Update webpack and add to vendors.json

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
-declare const __IS_BUILD__: boolean, __IS_WEBWORKER__: booleanimport 'rxjs/add/operator/filter'import {bootloader} from '@angularclass/hmr'import {enableProdMode} from '@angular/core'import {platformBrowserDynamic} from '@angular/platform-browser-dynamic'import AppModule from 'app'const main = () => platformBrowserDynamic()  .bootstrapModule(AppModule)if (__IS_BUILD__) {  enableProdMode()  main()} else {  bootloader(main)}
+declare const __IS_BUILD__: boolean, __IS_WEBWORKER__: boolean
+
+import 'rxjs/add/operator/filter'
+
+import {bootloader} from '@angularclass/hmr'
+import {enableProdMode} from '@angular/core'
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic'
+
+import AppModule from 'app'
+
+const main = () => platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+
+if (__IS_BUILD__) {
+  enableProdMode()
+  main()
+} else {
+  bootloader(main)
+}
